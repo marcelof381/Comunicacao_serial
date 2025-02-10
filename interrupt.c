18ef8b4f954131629bcbfa9831ff022b957f998d
Marcelo Freitas <marcelofreitas381@gmail.com>
2025-02-09 22:49:06 -0300

    Update interrupt.c

--- interrupt.c
+++ interrupt.c
@@ -5,6 +5,14 @@
 #include "hardware/clocks.h"
 #include "hardware/gpio.h"
 
+#include "hardware/i2c.h"
+#include "inc/ssd1306.h"
+#include "inc/font.h"
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
+
 #include "animacao_matriz.pio.h"  // Programa PIO compilado para controle dos LEDs
 
 // ======================= CONSTANTES ========================
@@ -22,6 +30,8 @@ uint maquina_estado;              // Índice da máquina de estado PIO
 volatile uint numero_atual = 0;   // Número exibido (0-9)
 volatile uint32_t ultimo_toque = 0; // Temporização para debounce
 const float fator_brilho = 0.8;   // 80% do brilho máximo (20% de redução)
+bool state_azul = false;
+bool state_verde = false;
 
 // ====================== CONFIGURAÇÕES FIXAS ==================
 // Padrões dos números (0-9) representados na matriz 5x5
@@ -121,16 +131,52 @@ void tratar_interrupcao_botao(uint gpio, uint32_t eventos) {
     if (tempo_atual - ultimo_toque > 200) {
         if (gpio == BOTAO_B) {
             gpio_put(LED_AZUL, !gpio_get(LED_AZUL));
+            state_azul = !LED_AZUL;
+            if (state_azul == 1){
+            printf("Led azul foi acionado \n");
+            }
+            else{
+            printf("Led azul foi desligado \n"); 
+            }
         } 
         else if (gpio == BOTAO_A) {
             gpio_put(LED_VERDE, !gpio_get(LED_VERDE));
+            if (LED_VERDE == 1){
+                printf("Led verde foi acionado \n");
+            }
+            else{
+                printf("Led verde foi desligado \n"); 
+            }
+
         }
     }
 }
 
+void desenho(){
+    
+}
 
 // ===================== PROGRAMA PRINCIPAL ====================
 int main() {
+
+      // I2C Initialisation. Using it at 400Khz.
+  i2c_init(I2C_PORT, 400 * 1000);
+
+  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+  gpio_pull_up(I2C_SDA); // Pull up the data line
+  gpio_pull_up(I2C_SCL); // Pull up the clock line
+  ssd1306_t ssd; // Inicializa a estrutura do display
+  ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+  ssd1306_config(&ssd); // Configura o display
+  ssd1306_send_data(&ssd); // Envia os dados para o display
+
+  // Limpa o display. O display inicia com todos os pixels apagados.
+  ssd1306_fill(&ssd, false);
+  ssd1306_send_data(&ssd);
+
+  bool cor = true;
+
     // Configuração inicial do sistema
     controlador_pio = pio0;  // Usa controlador PIO 0
     bool clock_01 = set_sys_clock_khz(128000, false);  // Clock a 128MHz
@@ -165,9 +211,22 @@ int main() {
     // ------ Inicialização da matriz ------
     atualizar_matriz(0);  // Começa exibindo o número 0
 
+    i2c_init(I2C_PORT, 400 * 1000);
+
+  
     // ------ Loop principal ------
     while (true) {
         
+                    // Atualiza o conteúdo do display com animações
+                    ssd1306_fill(&ssd, !cor); // Limpa o display
+                    ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
+                    ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 10); // Desenha uma string
+                    ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30); // Desenha uma string
+                    ssd1306_draw_string(&ssd, "azeiTona", 15, 48); // Desenha uma string      
+                    ssd1306_send_data(&ssd); // Atualiza o display
+                
+                    sleep_ms(1000);
+
          // Certifica-se de que o USB está conectado
             char c;
             scanf("%c", &c);
@@ -177,5 +236,6 @@ int main() {
                 atualizar_matriz(numero_atual);
             }
             sleep_ms(40);
+            cor = !cor;
          }
 }
